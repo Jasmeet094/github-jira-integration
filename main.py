a1a371ce816ec68e864c09429b87c7d7c4f08654
Jasmeet094 <115403345+Jasmeet094@users.noreply.github.com>
2024-03-18 17:25:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,70 @@
 from flask import Flask
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+# create flask app instance 
 
 app = Flask(__name__)
 
-@app.route('/JiraCreate'. methods=['POST'])
+@app.route("/JiraCreate". methods=['POST'])
+
 def createjira():
     
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        url = "https://jasmeet.atlassian.net/rest/api/3/issue"
+
+        auth = HTTPBasicAuth("cikac29946@fryshare.com", "ATATT3xFfGF0__UYe6YCCc4eMGUILfM8s3pxEkkrrh7guHxLI_PVYRwCjxg8ShOgc2HukaPiFIhjDDI1XdhS73bYekerQvuxu58e6DGvx6HLddEMqbXNSxRNUeT2FlrSTvM9dtP3XOaV48-8hoeTKPHDsoD_R0l9gO5EGQx2qOeCEYX_9itQeOM=8BAD092C")
+
+        headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+        }
+
+        payload = json.dumps( {
+        "fields": {
+        
+        
+
+            "description": {
+            "content": [
+                {
+                "content": [
+                    {
+                    "text": "my first ticket.",
+                    "type": "text"
+                    }
+                ],
+                "type": "paragraph"
+                }
+            ],
+            "type": "doc",
+            "version": 1
+            },
+        
+            "issuetype": {
+            "id": "10001"
+            },
+
+            "project": {
+            "key": "MFK"
+            },
+        
+            "summary": "DEMO Ticket",
+            
+        },
+        "update": {}
+        } )
+
+        response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+        )
+
+        return(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+
+app.run('0.0.0.0' , port=5000)
