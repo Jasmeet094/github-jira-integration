b0c201a7c3f94cb7689dd7b00c77aec6d50d0e25
Jasmeet094 <115403345+Jasmeet094@users.noreply.github.com>
2024-03-18 18:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,68 +3,67 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-# create flask app instance 
+# create flask app instance
 
 app = Flask(__name__)
 
-@app.route("/JiraCreate". methods=['POST'])
+@app.route("/CreateJira", methods=['POST'])
 
 def createjira():
-    
 
-        url = "https://jasmeet.atlassian.net/rest/api/3/issue"
+    url = "https://jasmeet.atlassian.net/rest/api/3/issue"
 
-        auth = HTTPBasicAuth("cikac29946@fryshare.com", "ATATT3xFfGF0__UYe6YCCc4eMGUILfM8s3pxEkkrrh7guHxLI_PVYRwCjxg8ShOgc2HukaPiFIhjDDI1XdhS73bYekerQvuxu58e6DGvx6HLddEMqbXNSxRNUeT2FlrSTvM9dtP3XOaV48-8hoeTKPHDsoD_R0l9gO5EGQx2qOeCEYX_9itQeOM=8BAD092C")
+    auth = HTTPBasicAuth("cikac29946@fryshare.com", "ATATT3xFfGF0qxPbR-PV-9L41fjHIpas-wb_uGMAUlhKZQK8luL0puP1r_tlnGw9tD06HH4hL73zMOMrkaXgjgNaks15zqWxv4cIIuv0Wfdp-K8gykU5ovjXmufbdnu9-LXeL5EUmKRRoNQtsVvRIs-zrx89FpmEgZytR4YVOmGUnrTt_c6ijYw=CA29E67F")
 
-        headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+    "fields": {
+
+
+
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "my first ticket.",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
         }
+      ],
+      "type": "doc",
+      "version": 1
+    },
 
-        payload = json.dumps( {
-        "fields": {
-        
-        
+    "issuetype": {
+      "id": "10001"
+    },
 
-            "description": {
-            "content": [
-                {
-                "content": [
-                    {
-                    "text": "my first ticket.",
-                    "type": "text"
-                    }
-                ],
-                "type": "paragraph"
-                }
-            ],
-            "type": "doc",
-            "version": 1
-            },
-        
-            "issuetype": {
-            "id": "10001"
-            },
+    "project": {
+      "key": "MFK"
+    },
 
-            "project": {
-            "key": "MFK"
-            },
-        
-            "summary": "DEMO Ticket",
-            
-        },
-        "update": {}
-        } )
+    "summary": "DEMO Ticket",
 
-        response = requests.request(
-        "POST",
-        url,
-        data=payload,
-        headers=headers,
-        auth=auth
-        )
+    },
+    "update": {}
+    } )
 
-        return(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    response = requests.request(
+    "POST",
+    url,
+    data=payload,
+    headers=headers,
+    auth=auth
+    )
+
+    return(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
 
 
 app.run('0.0.0.0' , port=5000)
